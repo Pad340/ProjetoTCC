db7354b77d78bd4e551c2d5cb0e771be9a80ba41
Bruno <brunorohling340@gmail.com>
2024-06-27 15:08:53 -0300

    Adicionei um arquivo de Configs, onde são definidas constantes que seram usadas na aplicação. Adicionei também o autoload para ajudar com o caminho de arquivos. Arrumei algumas pastas e criei arquivos, ainda sem conteúdo, para area logada e area web mesmo.

--- index.php
+++ index.php
@@ -2,23 +2,12 @@
 // Iniciar a sessão
 session_start();
 
-// Definir o fuso horário padrão
-date_default_timezone_set('America/Sao_Paulo');
+include 'vendor/autoload.php';
 
 // Configurações de erro (desativar em produção)
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-// Incluir o autoload do Composer
-//require 'vendor/autoload.php';
-
-// Criar uma instância de conexão com o banco de dados
-//$db = new Core\Connect();
-
-// Definir constantes para caminhos
-const BASE_URL = 'http://localhost/ProjetoTCC/';
-const CSS_PATH = BASE_URL . 'shared/styles/';
 
 ?>
 <!DOCTYPE html>
---
+++ source/Boot/Config.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * DATABASE
+ */
+const DB_HOST = 'localhost';
+const DB_USER = 'root';
+const DB_PASS = '';
+const DB_NAME = 'projeto_tcc';
+const DB_CHARSET = 'utf8mb4';
+
+/**
+ * PROJECT URLs
+ */
+const URL_BASE = 'https://www.projetotcc.com';
+const URL_TEST = 'https://localhost/ProjetoTCC';
+
+/**
+ * SITE
+ */
+const SITE_NAME = 'ProjetoTCC';
+const SITE_TITLE = 'A definir';
+const SITE_DESC = 'A decidir';
+const SITE_LANG = 'pt_BR';
+
+/**
+ * SOCIAL
+ */
+
+
+/**
+ * DATES
+ */
+const DATE_BR = 'd/m/Y H:i:s';
+const DATE_TIME_FORMAT = 'Y-m-d H:i:s';
