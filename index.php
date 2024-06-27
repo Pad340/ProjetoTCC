1ec557dce566a345d63a94056a339f4d34f024c8
Bruno <brunorohling340@gmail.com>
2024-06-27 17:18:03 -0300

    Tentativa de criar um url amigavel

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto TCC</title>
+</head>
+<body>
 <?php
-// Iniciar a sessão
-session_start();
 
 include 'vendor/autoload.php';
 
@@ -9,21 +15,16 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+$url = (isset($_GET['url'])) ? $_GET['url'] : 'home.php';
+$url = array_filter(explode('/', $url));
+
+$file = $url[0] . '.php';
+
+if (is_file($file)) {
+    include $file;
+} else {
+    echo 'erro';
+}
 ?>
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projeto TCC</title>
-    <link rel="stylesheet" href="shared/styles/styles.css">
-</head>
-<body>
-<?php include 'shared/views/header.php'; ?>
-
-<h1>Bem-vindo ao meu TCC</h1>
-<p>Esta é a página inicial do meu projeto de TCC.</p>
-
-<?php include 'shared/views/footer.php'; ?>
 </body>
 </html>
--- themes/web/header.php
+++ themes/web/header.php
@@ -3,8 +3,6 @@
         <nav>
             <ul>
                 <li><a href="">Home</a></li>
-                <li><a href="#">Sobre</a></li>
-                <li><a href="#">Contato</a></li>
             </ul>
         </nav>
     </header>
