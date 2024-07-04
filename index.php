c20eb1bc9a8cba833d1198abfb604f81caa8d7be
Bruno <brunorohling340@gmail.com>
2024-07-04 10:47:05 -0300

    Removendo lixos do index

--- index.php
+++ index.php
@@ -10,21 +10,7 @@
 
 include 'vendor/autoload.php';
 
-// Configurações de erro (desativar em produção)
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
-$url = (isset($_GET['url'])) ? $_GET['url'] : 'home.php';
-$url = array_filter(explode('/', $url));
-
-$file = $url[0] . '.php';
-
-if (is_file($file)) {
-    include $file;
-} else {
-    echo 'erro';
-}
 ?>
 </body>
 </html>
