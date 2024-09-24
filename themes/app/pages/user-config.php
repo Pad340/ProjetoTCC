044ba8576ad9be22783c280ab27bb890984934db
bagoz <bagoz@mail.com>
2024-09-24 10:06:30 -0300

    user config parcialmente pronto :3

--- themes/app/pages/user-config.php
+++ themes/app/pages/user-config.php
@@ -42,44 +42,48 @@ $seller_result = $search->selectFirst('seller', 'WHERE user_id = :id', "id={$ses
                 <h2>E-mail</h2>
                 <p><?= $user_result['email'] ?></p>
             </div>
-            <div class="UC-text-div">
+            <div class="UC-member-div">
                 <h2>Membro desde</h2>
                 <p><?= date_fmt($user_result['created_at'], 'd/m/Y') ?></p>
             </div>
         </div>
-
-        <?php
-        if (!$session->has('authSeller') and !$seller_result) {
-        ?>
-
+    </div>
+    <?php
+    if (!$session->has('authSeller') and !$seller_result) {
+    ?>
+        <div class="SC-container">
             <div class="seller-form">
-                <h2>Criar uma conta de vendedor</h2>
+                <h1 id="UC-text">Criar uma conta de vendedor</h1>
                 <form action="" method="post" autocomplete="off">
-                    <label for="seller_name">Nome de vendedor ou turma:</label>
-                    <input type="text" name="seller_name" id="seller_name" required />
+                    <label for="seller_name">Nome de vendedor ou turma</label> <br>
+                    <input type="text" name="seller_name" id="seller_name" required /> <br>
 
-                    <label for="seller_cpf">CPF do vendedor ou responsável pelas vendas da turma:</label>
-                    <input type="text" name="seller_cpf" id="seller_cpf" maxlength="14" required />
+                    <label for="seller_cpf">CPF do vendedor ou responsável pelas vendas da turma</label> <br>
+                    <input type="text" name="seller_cpf" id="seller_cpf" maxlength="14" required /> <br>
 
-                    <label for="phone_number">Número de telefone do vendedor ou responsável pelas vendas da turma:</label>
-                    <input type="text" name="phone_number" id="phone_number" maxlength="15" required />
+                    <label for="phone_number">Número de telefone do vendedor ou responsável pelas vendas da turma</label> <br>
+                    <input type="text" name="phone_number" id="phone_number" maxlength="15" required /> <br>
 
                     <button id="UC-button" type="submit" name="register_btn">Cadastrar</button>
                 </form>
             </div>
+        </div>
+    <?php
+    } elseif ($seller_result['status_account'] == 0) {
+    ?>
 
-        <?php
-        } elseif ($seller_result['status_account'] == 0) {
-        ?>
-            <form action="" method="post">
-                <button id="UC-button" type="submit" name="enable_btn">Reativar conta de vendedor</button>
-            </form>
-        <?php
-        } else {
-        ?>
+        <form action="" method="post">
+            <button id="UC-button" type="submit" name="enable_btn">Reativar conta de vendedor</button>
+        </form>
+
+    <?php
+    } else {
+    ?>
+        <div class="SC-container">
             <div class="UC-info">
+                <h1 id="UC-text">Configurações do vendedor</h1>
                 <div class="UC-text-div">
-                    <h2>Vendedor</h2>
+                    <h2>Nome do Vendedor</h2>
                     <p><?= $seller_result['name'] ?></p>
                 </div>
                 <div class="UC-text-div">
@@ -94,27 +98,26 @@ $seller_result = $search->selectFirst('seller', 'WHERE user_id = :id', "id={$ses
             <form action="" method="post">
                 <button id="UC-button" type="submit" name="disable_btn">Desativar conta de vendedor</button>
             </form>
+        </div>
+    <?php
+    }
+    ?>
+    <script>
+        $(document).ready(function() {
+            // Máscara para CPF
+            $('#seller_cpf').mask('000.000.000-00');
 
-        <?php
-        }
-        ?>
-        <script>
-            $(document).ready(function() {
-                // Máscara para CPF
-                $('#seller_cpf').mask('000.000.000-00');
+            // Máscara para o número de telefone
+            const phoneMaskBehavior = function(val) {
+                    return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+                },
+                phoneOptions = {
+                    onKeyPress: function(val, e, field, options) {
+                        field.mask(phoneMaskBehavior.apply({}, arguments), options);
+                    }
+                };
 
-                // Máscara para o número de telefone
-                const phoneMaskBehavior = function(val) {
-                        return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
-                    },
-                    phoneOptions = {
-                        onKeyPress: function(val, e, field, options) {
-                            field.mask(phoneMaskBehavior.apply({}, arguments), options);
-                        }
-                    };
-
-                $('#phone_number').mask(phoneMaskBehavior, phoneOptions);
-            });
-        </script>
-    </div>
+            $('#phone_number').mask(phoneMaskBehavior, phoneOptions);
+        });
+    </script>
 </div>
