ce24dadbb0177aa64ba2da41438d9bea13983af9
bago <email@gmail.com>
2024-10-11 11:59:28 -0300

    botei um template decente do slider q peguei na net

--- themes/app/pages/home.php
+++ themes/app/pages/home.php
@@ -21,7 +21,50 @@ $bestSellingProducts = $search->executeQuery('
 
 ?>
 
+
+
 <div class="home">
+
+    <div id="slider">
+        <input type="radio" name="slider" id="slide1" checked>
+        <input type="radio" name="slider" id="slide2">
+        <input type="radio" name="slider" id="slide3">
+        <input type="radio" name="slider" id="slide4">
+        <div id="slides">
+            <div id="overflow">
+                <div class="inner">
+                    <div class="slide slide_1">
+                        <div class="slide-content">
+                            <h2>Slide 1</h2>
+                        </div>
+                    </div>
+                    <div class="slide slide_2">
+                        <div class="slide-content">
+                            <h2>Slide 2</h2>
+                        </div>
+                    </div>
+                    <div class="slide slide_3">
+                        <div class="slide-content">
+                            <h2>Slide 3</h2>
+                        </div>
+                    </div>
+                    <div class="slide slide_4">
+                        <div class="slide-content">
+                            <h2>Slide 4</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="bullets">
+            <label for="slide1"></label>
+            <label for="slide2"></label>
+            <label for="slide3"></label>
+            <label for="slide4"></label>
+        </div>
+    </div>
+
 
     <div class="title">
         <h2>Bem vindo <?= $session->username ?>!</h2>
