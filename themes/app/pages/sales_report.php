6005de1ead77260cb21e77fa55b43f85e5e5f7c0
Bruno <brunorohling340@gmail.com>
2024-12-05 00:53:35 -0300

    correção de estrutura

--- themes/app/pages/sales_report.php
+++ themes/app/pages/sales_report.php
@@ -28,17 +28,17 @@ $lastReserve = null;
     <?php } else { ?>
         <table class="sales-report-table">
             <thead>
-                <tr>
-                    <th>Data e Hora</th>
-                    <th>ID da reserva</th>
-                    <th>Total da reserva</th>
-                    <th>Produto</th>
-                    <th>Preço Unitário</th>
-                    <th>Quantidade</th>
-                    <th>Total (Preço x Qnt.)</th>
-                    <th>Reservado para</th>
-                    <th>Foi retirado?</th>
-                </tr>
+            <tr>
+                <th>Data e Hora</th>
+                <th>ID da reserva</th>
+                <th>Total da reserva</th>
+                <th>Produto</th>
+                <th>Preço Unitário</th>
+                <th>Quantidade</th>
+                <th>Total (Preço x Qnt.)</th>
+                <th>Reservado para</th>
+                <th>Foi retirado?</th>
+            </tr>
             </thead>
 
             <?php foreach ($reports as $report) { ?>
@@ -55,51 +55,6 @@ $lastReserve = null;
                 </tr>
             <?php } ?>
         </table>
-</div>
 
-<!--        --><?php //foreach ($reports as $report) { 
-                ?>
-<!--            <div class="data">-->
-<!--                --><?php
-                        //                if ($lastDate != date_fmt($report['reserved_at'], 'd/m/Y')) {
-                        //                    $lastDate = date_fmt($report['reserved_at'], 'd/m/Y'); 
-                        ?>
-<!---->
-<!--                    <div class='date'>-->
-<!--                        <h2>--><?php //= $lastDate 
-                                    ?><!--</h2>-->
-<!--                    </div>-->
-<!--                --><?php //} 
-                        ?>
-<!---->
-<!--                --><?php //if ($lastReserve != $report['reserve_id']) {
-                        //                    $lastReserve = $report['reserve_id']; 
-                        ?>
-<!--                    <h3>ID da reserva: --><?php //= $lastReserve 
-                                                ?><!--</h3>-->
-<!--                    <h3>Total da reserva: --><?php //= $report['total_value'] 
-                                                    ?><!--</h3>-->
-<!--                --><?php //} 
-                        ?>
-<!---->
-<!--                <div class="sales_data" style="border: black 1px solid; margin: 1px">-->
-<!--                    <p>--><?php //= $report['product_name'] 
-                                ?><!--</p>-->
-<!--                    <p>Preço Un.: R$-->
-<!--                        --><?php //= brl_price_format($report['value'] / $report['quantity']) 
-                                ?><!--</p>-->
-<!--                    <p>Quantidade: --><?php //= $report['quantity'] 
-                                            ?><!--</p>-->
-<!--                    <p>Total (preço x quantidade): --><?php //= $report['value'] 
-                                                            ?><!--</p>-->
-<!--                    <p>Reservado para:-->
-<!--                        --><?php //= $search->selectFirst('user', 'WHERE user_id = :id', "id={$report['user_id']}", 'name')['name'] 
-                                ?>
-<!--                    </p>-->
-<!--                    <p>Produto foi retirado? --><?php //= $report['redeemed'] == 1 ? 'Sim' : 'Não' 
-                                                    ?><!--</p>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--        --><?php //} 
-                ?>
-<?php } ?>
+    <?php } ?>
+</div>
